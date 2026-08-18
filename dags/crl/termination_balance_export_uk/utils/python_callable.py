cfb468eec06413ec99d57a6558f6bc535031075c
repl-muhammed-abnas <muhammed.abnas@replicon.com>
2026-08-18 18:29:59 +0530

    minor changes

--- dags/crl/termination_balance_export_uk/utils/python_callable.py
+++
@@ -1,9 +0,0 @@
-import pendulum
-
-def get_time_in_formats(time_zone):
-    current_time = pendulum.now(time_zone)
-    return {
-        "start_time": str(current_time),
-        "ymd_format": current_time.strftime("%Y%m%d"),
-        "hms_format": current_time.strftime("%H%M%S")
-    }
